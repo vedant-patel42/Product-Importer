fb4d2818709aa4ba252c98bae116eb355b265473
Vedant <vn98patel@gmail.com>
2025-11-25 17:03:48 +0530

    Fixing toml file

--- api/main.py
+++ api/main.py
@@ -256,4 +256,5 @@ async def test_webhook(url: str = Query(..., description="The URL to test")):
 
 #Run Server
 if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
